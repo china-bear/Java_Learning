cea7cd7ccab7d0d842da1d62602ce75ac871dde4
YOUGUO XIONG <xiongyouguo@360.cn>
2020-09-04 19:43:07 +0800

    多线程例子

--- code/Annotation/src/main/java/demo/Test.java
+++ code/Annotation/src/main/java/demo/Test.java
@@ -4,6 +4,9 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 /**
+ * https://juejin.im/post/6844903636733001741
+ * https://www.jianshu.com/p/28edf5352b63
+ * http://www.akathink.com/2016/08/11/%E5%BD%BB%E5%BA%95%E6%90%9E%E6%87%82Java%E6%B3%A8%E8%A7%A3Annotation/
  * Created by bear on 2020/9/4
  */
 public class Test {

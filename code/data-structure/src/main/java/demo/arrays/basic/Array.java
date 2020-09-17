bedd77267fc48411501adbceadae888519bec36b
YOUGUO XIONG <xiongyouguo@360.cn>
2020-09-17 14:15:40 +0800

    跳表

--- code/data-structure/src/main/java/demo/arrays/basic/Array.java
+++ code/data-structure/src/main/java/demo/arrays/basic/Array.java
@@ -15,14 +15,16 @@ public class Array {
         System.out.println("------------------------------");
         int[] scores = new int[]{100, 99, 66};
         for(int i = 0 ; i < scores.length ; i ++)
-            System.out.println(scores[i]);
+            System.out.print(scores[i] + "\t");
+            System.out.println();
 
         for(int score: scores)
-            System.out.println(score);
+            System.out.print(score + "\t");
+            System.out.println();
 
         scores[0] = 96;
         System.out.println("------------------------------");
         for(int i = 0 ; i < scores.length ; i ++)
-            System.out.println(scores[i]);
+            System.out.print(scores[i] + "\t");
     }
 }

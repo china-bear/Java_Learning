8b5890033e65f78820a06cdb40221e1d4e5d00c8
YOUGUO XIONG <xiongyouguo@360.cn>
2020-09-29 15:55:32 +0800

    数据结构实现

---
+++ code/data-structure/src/main/java/linear_data_structure/List/MainClass.java
@@ -0,0 +1,71 @@
+package linear_data_structure.List;
+
+/**
+ * Created by bear on 2020/9/9
+ */
+public class MainClass {
+
+    public static void main(String[] args) throws Exception {
+        LinkedList<String> strList = new LinkedList<String>();
+        strList.add("Rico");
+        strList.add("Rico");
+        strList.add("Livia");
+        strList.add("TJU");
+        strList.add("Livia");
+        strList.add("NEU");
+        strList.add("NEU");
+        strList.add("Rico");
+        strList.add("NEU");
+
+        System.out.println("原链表：");
+        strList.print();
+        System.out.println();
+
+        strList.remove();
+        System.out.println("删除尾节点后的链表：");
+        strList.print();
+
+        System.out.println();
+
+        strList.removeDuplicateNodes();
+        System.out.println("删除重复点后的链表：");
+        strList.print();
+        System.out.println();
+
+        System.out.println("打印倒数第K个节点：");
+        System.out.println(strList.getEndK(2));
+        System.out.println();
+
+        strList.reverseLinkedList();
+        System.out.println("反转链表：");
+        strList.print();
+        strList.reverseLinkedList();
+        strList.print();
+        System.out.println();
+
+        System.out.println("倒序打印链表：");
+        strList.reversePrint(strList.getHead());
+        System.out.println();
+        System.out.println();
+
+        System.out.println("打印链表中间节点：");
+        strList.printMiddleNodes();
+        System.out.println();
+
+
+        LinkedList<String> strList2 = new LinkedList<String>();
+        strList2.add("书呆子");
+        strList2.add(strList.getEndK(2));
+        System.out.println("判断两链表是否相交：");
+        strList.print();
+        strList2.print();
+        System.out.println(strList.size() + " : " + strList2.size());
+        System.out.println("是否相交 : " + strList.isIntersect(strList2));
+        System.out.println("交点是 ： " + strList.getIntersectionPoint(strList2));
+        System.out.println();
+
+        strList.deleteNodeWithoutHead(strList.getEndK(2));
+        System.out.println("打印删除特定节点后的链表：");
+        strList.print();
+    }
+}
---
+++ code/data-structure/src/test/java/linear_data_structure/List/doubleLinked/DoublyLinkedListTest.java
@@ -0,0 +1,155 @@
+/**
+ * Data-Structures-In-Java
+ * DoublyLinkedListTest.java
+ */
+package linear_data_structure.List.doubleLinked;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test cases for Doubly linked list
+ * 
+ * @author Deepak
+ */
+public class DoublyLinkedListTest {
+
+	/**
+	 * Test case to check insertion at head of linked list
+	 */
+	@Test
+	public void testInsertionAtHead() {
+		DoublyLinkedList<String> doublyList = new DoublyLinkedList<>();
+		assertTrue(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 0);
+		doublyList.insertAtHead("Tom");
+		assertFalse(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 1);
+		assertEquals(doublyList.searchByIndex(0).item, "Tom");
+		doublyList.insertAtHead("Alex");
+		assertTrue(doublyList.size() == 2);
+		assertEquals(doublyList.searchByIndex(0).item, "Alex");
+	}
+
+	/**
+	 * Test case to check insertion at tail of linked list
+	 */
+	@Test
+	public void testInsertionAtTail() {
+		DoublyLinkedList<String> doublyList = new DoublyLinkedList<>();
+		assertTrue(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 0);
+		doublyList.insertAtHead("Tom");
+		assertFalse(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 1);
+		assertEquals(doublyList.searchByIndex(0).item, "Tom");
+		doublyList.insertAtTail("Alex");
+		assertTrue(doublyList.size() == 2);
+		assertEquals(doublyList.searchByIndex(0).item, "Tom");
+		assertEquals(doublyList.searchByIndex(1).item, "Alex"); 
+	}
+
+	/**
+	 * Test case to check insertion at a given position of linked list
+	 */
+	@Test
+	public void testInsertionAtPosition() {
+		DoublyLinkedList<String> doublyList = new DoublyLinkedList<>();
+		assertTrue(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 0);
+		doublyList.insertAtHead("Tom");
+		doublyList.insertAtHead("Alex");
+		doublyList.insertAtHead("Maria");
+		assertFalse(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 3);
+		assertEquals(doublyList.searchByIndex(0).item, "Maria");
+		assertEquals(doublyList.searchByValue("Maria").item, "Maria");
+		doublyList.insertAtPosition("Steve", 2);
+		assertTrue(doublyList.size() == 4);
+		assertEquals(doublyList.searchByIndex(1).item, "Alex");
+		assertEquals(doublyList.searchByIndex(2).item, "Tom");
+		assertEquals(doublyList.searchByIndex(3).item, "Steve");
+	}
+
+	/**
+	 * Test case to check deletion from head
+	 */
+	@Test
+	public void testDeleteFromHead() {
+		DoublyLinkedList<String> doublyList = new DoublyLinkedList<>();
+		assertTrue(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 0);
+		doublyList.insertAtHead("Tom");
+		doublyList.insertAtHead("Alex");
+		doublyList.insertAtHead("Maria");
+		assertFalse(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 3);
+		assertEquals(doublyList.searchByIndex(0).item, "Maria");
+		doublyList.deleteFromHead();
+		assertTrue(doublyList.size() == 2);
+		assertEquals(doublyList.searchByIndex(0).item, "Alex");
+	}
+
+	/**
+	 * Test case to check deletion from tail
+	 */
+	@Test
+	public void testDeleteFromTail() {
+		DoublyLinkedList<String> doublyList = new DoublyLinkedList<>();
+		assertTrue(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 0);
+		doublyList.insertAtHead("Tom");
+		doublyList.insertAtHead("Alex");
+		doublyList.insertAtHead("Maria");
+		assertFalse(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 3);
+		assertEquals(doublyList.searchByIndex(2).item, "Tom");
+		doublyList.deleteFromTail();
+		assertTrue(doublyList.size() == 2);
+		assertEquals(doublyList.searchByIndex(0).item, "Maria");
+		assertEquals(doublyList.searchByIndex(1).item, "Alex");
+	}
+
+	/**
+	 * Test case to check deletion from position
+	 */
+	@Test
+	public void testDeleteFromPosition() {
+		DoublyLinkedList<String> doublyList = new DoublyLinkedList<>();
+		assertTrue(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 0);
+		doublyList.insertAtHead("Tom");
+		doublyList.insertAtHead("Alex");
+		doublyList.insertAtHead("Maria");
+		assertFalse(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 3);
+		assertEquals(doublyList.searchByIndex(2).item, "Tom");
+		doublyList.deleteFromPosition(1);
+		assertTrue(doublyList.size() == 2);
+		assertEquals(doublyList.searchByIndex(1).item, "Tom");
+	}
+
+	/**
+	 * Test case to convert a linked list to Array
+	 */
+	@Test
+	public void testConvertToArray() {
+		DoublyLinkedList<String> doublyList = new DoublyLinkedList<>();
+		assertTrue(doublyList.isEmpty());
+		assertTrue(doublyList.size() == 0);
+		doublyList.insertAtHead("Tom");
+		doublyList.insertAtHead("Alex");
+		doublyList.insertAtHead("Maria");
+		assertTrue(doublyList.size() == 3);
+		Object[] array = doublyList.toArray();
+		assertTrue(array.length == 3);
+		assertEquals(array[0], "Maria");
+		assertEquals(array[1], "Alex");
+		assertEquals(array[2], "Tom");
+	}
+
+}

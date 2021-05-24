d1c7cb0b5d32b916047b7e8e6c4a9717332455ad
YOUGUO XIONG <xiongyouguo@360.cn>
2021-05-24 10:51:13 +0800

    静态代码块的执行顺序

---
+++ code/basic/src/main/java/classTest/StaticTest01.java
@@ -0,0 +1,58 @@
+package classTest;
+
+/** 类的初始化顺序依次是
+ * 加载父类的静态字段 -> 父类的静态代码块 -> 子类静态字段 -> 子类静态代码块 -> 父类成员变量（非静态字段）-> 父类非静态代码块 -> 父类构造器 -> 子类成员变量 -> 子类成员变量 -> 子类非静态代码块 -> 子类构造器
+ * static 修饰的变量存储在方法区中???   static 变量的生命周期与类的生命周期相同，随类的加载而创建，随类的销毁而销毁???
+ * 声明为 static 和 transient 类型的变量不能被序列化，因为 static 修饰的变量保存在方法区中
+ * 对于每个 StaticTest 的实例化对象都会拥有一个 LOGGER,通常会将 Logger 对象声明为 static 变量，能够减少对内存资源的占用。
+ **/
+public class StaticTest01 {
+
+    public StaticTest01() {
+        System.out.print("默认构造方法！-->");
+    }
+
+    //非静态代码块
+    {
+        System.out.print("非静态代码块！-->");
+    }
+
+    //静态代码块  具有最高的优先级
+    // 类加载的时候，就被执行了
+    static {
+        System.out.print("静态代码块！-->");
+    }
+
+    private static void test1() {
+        System.out.print("静态方法test1中的内容1! -->");
+        {
+            System.out.print("静态方法test1中的代码块！-->");
+        }
+        System.out.print("静态方法test1中的内容2! -->");
+    }
+
+    private  void test2() {
+        System.out.print("方法test2中的内容1! -->");
+        {
+            System.out.print("方法test2中的代码块！-->");
+        }
+        System.out.print("方法test2中的内容2! -->");
+    }
+
+    public static void main(String[] args) {
+        // 类方法 直接调用
+        test1();  //静态代码块！-->静态方法test1中的内容1! -->静态方法test1中的代码块！-->静态方法test1中的内容2! -->
+
+        System.out.println();System.out.println();
+
+        StaticTest01 staticTest = new StaticTest01();  // 非静态代码块！-->默认构造方法！-->
+
+        System.out.println();System.out.println();
+
+        staticTest.test1();//静态方法test1中的内容1! -->静态方法test1中的代码块！-->静态方法test1中的内容2! -->
+
+        System.out.println();System.out.println();
+
+        staticTest.test2();//方法test2中的内容1! -->方法test2中的代码块！-->方法test2中的内容2! -->
+    }
+}
---
+++ code/basic/src/main/java/classTest/StaticTest02.java
@@ -0,0 +1,50 @@
+package classTest;
+
+/**
+* 在new一个对象之前，需要先将类加载到内存中，所以会一直找到最顶层父类Object加载，依次往下加载父类，由于静态代码块是随着类的加载而执行的，所以会先输出“XXX静态代码块”；
+* 加载完之后，由于非静态代码的执行顺序是高于构造器方法体内容的，所以，先执行“XXX非静态方法块”后执行“XXX构造器”；
+* 在new一个对象时，因为已经加载过了，所以只执行非静态代码块和构造器内容
+ **/
+public class StaticTest02 {
+    public static void main(String[] args) {
+        Leaf leaf1 = new Leaf();
+        System.out.println("****************************");
+        Leaf leaf2 = new Leaf();
+    }
+}
+
+class Root{
+    static {
+        System.out.println("Root静态代码块");
+    }
+    {
+        System.out.println("Root非静态代码块");
+    }
+    public Root() {
+        System.out.println("Root构造器");
+    }
+}
+
+class Mid extends Root{
+    static {
+        System.out.println("Mid静态代码块");
+    }
+    {
+        System.out.println("Mid非静态代码块");
+    }
+    public Mid() {
+        System.out.println("Mid构造器");
+    }
+}
+
+class Leaf extends Mid{
+    static {
+        System.out.println("Leaf静态代码块");
+    }
+    {
+        System.out.println("Leaf非静态代码块");
+    }
+    public Leaf() {
+        System.out.println("Leaf构造器");
+    }
+}

1e70846d67a8d7b9f8f3a170fca58d9ab7d97793
YOUGUO XIONG <xiongyouguo@360.cn>
2021-09-06 16:25:15 +0800

    JAVA设计模式

---
+++ code/design-patterns/proxy/src/main/java/station/cglib_proxy/TrainStation.java
@@ -0,0 +1,15 @@
+package station.cglib_proxy;
+
+
+/**
+ * @version v1.0
+ * @ClassName: TrainStation
+ * @Description: 火车站类
+ * @Author:
+ */
+public class TrainStation {
+
+    public void sell() {
+        System.out.println("火车站卖票");
+    }
+}
---
+++ code/design-patterns/proxy/src/main/java/station/static_proxy/SellTickets.java
@@ -0,0 +1,12 @@
+package station.static_proxy;
+
+/**
+ * @version v1.0
+ * @ClassName: SellTickets
+ * @Description: 卖火车票的接口
+ * @Author:
+ */
+public interface SellTickets {
+
+    void sell();
+}

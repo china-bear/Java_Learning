7dda41f3ff049d1e902d8971d16d3d86aa4add63
YOUGUO XIONG <xiongyouguo@360.cn>
2021-09-29 19:42:07 +0800

    JAVA 文档例子

--- code/functional-programming/src/main/java/java8_features/methodref/MethodReferenceExample.java
+++ code/functional-programming/src/main/java/java8_features/methodref/MethodReferenceExample.java
@@ -7,9 +7,13 @@ public class MethodReferenceExample {
 	// Classname :: methodName
 
 	public static void main(String[] args) {
-		System.out.println("Result 1 :"+MethodReferenceExample.compare(10, 20));
-		Comparator<Integer> comp =  MethodReferenceExample::compare;
-		System.out.println("Result 2:"+comp.compare(10, 20));
+		System.out.println("Result: "+MethodReferenceExample.compare(10, 20));
+
+		Comparator<Integer> comp1 =  MethodReferenceExample::compare;
+		System.out.println("Result: "+comp1.compare(10, 20));
+
+		Comparator<Integer> comp2 =  (x, y) ->  Integer.compare(x, y);
+		System.out.println("Result: "+comp2.compare(18, 6));
 	}
 	
 	public static int compare(Integer x,Integer y) {
